60270f292740daa07924418613d20feb32b05f07
William Lenoir <william.lenoir@etu.utc.fr>
2019-03-27 13:38:40 +0100

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,11 @@
 Creation des tables
 
+A corriger :
+
+- nom d'une table commence par une majuscule
+- manque des virgules parfois
+- vérifier que des types équivalents ont été mis à chaque fois (notamment pour les clefs étrangères), parfois les varchar n'ont pas de limite de caractères
+
 /* 1 <Floriane> De plus correction du prof a faire ! */
 
 CREATE TABLE client ( 
@@ -61,6 +67,33 @@ CREATE TABLE medicament (
 
 /* 4 <WILLIAM> */ 
 
-Autorisation Medicament
-Traitement
-Prescription
+CREATE TABLE AutorisationMedicament (
+    medicament VARCHAR(100),
+    espece VARCHAR(100),
+    PRIMARY KEY(medicament, espece),
+    FOREIGN KEY(medicament) REFERENCES medicament(nomMolecule),
+    FOREIGN KEY(espece) REFERENCES espece(nom)
+
+);
+
+CREATE TABLE Traitement (
+    id INTEGER AUTO_INCREMENT, 
+    debut DATE NOT NULL,
+    animal INTEGER NOT NULL,
+    duree INTEGER NOT NULL,
+    veterinaire INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(animal) REFERENCES animal(id),
+    FOREIGN KEY(veterinaire) REFERENCES veterinaire(id),
+    CONSTRAINT chk_duree CHECK (duree > 0)
+);
+
+CREATE TABLE Prescription (
+    medicament VARCHAR(100),
+    traitement INTEGER,
+    quantite INTEGER NOT NULL,
+    PRIMARY KEY(medicament, traitement),
+    FOREIGN KEY(medicament) REFERENCES medicament(nomMolecule),
+    FOREIGN KEY(traitement) REFERENCES Traitement(id),
+    CONSTRAINT chk_quantite CHECK (quantite > 0)
+);
