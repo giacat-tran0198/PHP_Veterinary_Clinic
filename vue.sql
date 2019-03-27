34f29f518beb0b96b899932246636bdecc59fad8
Floriane Allaire <floriane.allaire@etu.utc.fr>
2019-03-27 20:19:57 +0100

    Delete vue.sql

--- vue.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW Clinique.VPersonnelMedical (nom,prenom,dateNaissance,adresse,numero)
-AS
-SELECT V.nom,V.prenom,V.dateNaissance,V.adresse,V.numero FROM Clinique.Veterinaire V
-UNION
-SELECT A.nom,A.prenom,A.dateNaissance,A.adresse,A.numero FROM Clinique.Assistant A;
