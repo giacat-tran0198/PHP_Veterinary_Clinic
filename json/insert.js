8e387064546bbe4b81f51e30b883a4944e0b8a8e
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-05-06 18:00:45 +0200

    ajout des 2 premiers animaux en js

--- json/insert.js
+++ json/insert.js
@@ -1,20 +1,89 @@
-db.Clinique.insert(
-  {
+// La racine ici est l'animal 
+// L'ordre générale à suivre 
+/* 
+- nom :
+- poids :
+- taille :
+- dateNaissance : 
+- proprietaire : 
+    - nom :
+    - prenom :
+    - dateNaissance :
+    - adresse :
+    - numero :
+- espece : 
+  - nom : 
+  - classe :
+- traitement : 
+  - debut : 
+  - duree :
+  - medicament : 
+    - nomMolecure : 
+    - description : 
+    - quantite : 
+*/ 
+
+db.Clinique.insert({
     nom : "Sunny",
+    poids : "6",  
+    taille : "40", 
+    dateNaissance : "2014-12-15",
     proprietaire :
     {
       nom : "Allaire",
       prenom:"Laurence",
       dateNaissance:"1969-02-04",
-      adresse:"Saint-Andre",
-      numero:"0643564829"}
+      adresse:"Saint-Molf",
+      numero:"0761444829"
     }, 
-    poids : "6",  
-    taille : "40", 
-    dateNaissance : "2014-12-15",
     espece : {
       nom : "chien", 
       classe : "mammifere"
-    } 
-  }
-)
+    }
+  })
+
+
+  db.Clinique.insert({
+    nom : "Filou",
+    poids : "5",  
+    taille : "45", 
+    proprietaire :
+    {
+      nom : "Guerin",
+      prenom:"Maurice",
+      dateNaissance:"1940-04-04",
+      adresse:"Saint-Andre",
+      numero:"0643564829"
+    }, 
+    espece : {
+      nom : "chat", 
+      classe : "mammifere"
+    }, 
+    traitement : [{
+      debut : "2019-08-15", 
+      duree : 10 ,
+      medicament : [{
+        nomMolecure : "ANIMAZEN", 
+        description : "Aliment complémentaire pour les périodes de stress chez les chiens, chats.", 
+        quantite : 3 
+      }]
+    }]
+  })
+
+  
+
+  // Izao 
+
+
+
+  // Cristale 
+
+
+
+  // Bob 
+
+
+  // Margerite 
+
+
+  // Création de qq requete 
