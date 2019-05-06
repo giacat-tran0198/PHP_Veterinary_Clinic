86e0af4db59f054239dbf69fafbd1dbba55aa4e9
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-05-06 17:34:47 +0200

    json insert

--- json/insert.js
+++ json/insert.js
@@ -1,8 +1,6 @@
 db.Clinique.insert(
-
   {
     nom : "Sunny",
-
     proprietaire :
     {
       nom : "Allaire",
@@ -10,9 +8,13 @@ db.Clinique.insert(
       dateNaissance:"1969-02-04",
       adresse:"Saint-Andre",
       numero:"0643564829"}
-    }
-
-
-
+    }, 
+    poids : "6",  
+    taille : "40", 
+    dateNaissance : "2014-12-15",
+    espece : {
+      nom : "chien", 
+      classe : "mammifere"
+    } 
   }
 )
