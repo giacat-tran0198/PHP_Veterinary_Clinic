29116714a74f9c3357db37abe5b9b60be878fcad
Anna Chevalier <anna.chevalier@etu.utc.fr>
2019-05-06 18:02:13 +0200

    Update insert.js

--- json/insert.js
+++ json/insert.js
@@ -6,26 +6,82 @@ db.Clinique.insert(
       nom : "Allaire",
       prenom:"Laurence",
       dateNaissance:"1969-02-04",
-      adresse:"Saint-Andre",
-      numero:"0643564829"}
+      adresse:"Saint-Molf",
+      numero:"0761444829"
     }, 
-    poids : "6",  
-    taille : "40", 
+    poids : 6,  
+    taille : 40, 
     dateNaissance : "2014-12-15",
     espece : {
       nom : "chien", 
       classe : "mammifere"
-    } 
+    },
     
-    traitement : [
+    traitement :
+    [
         
-        {medicament : "",
-         quantite : "",
-         date : ""
+        {
+            medicament : "",
+            quantite : "",
+            date : ""
         }
-        
         
         
         ]
   }
+)
+
+
+
+
+
+db.Clinique.insert(
+    
+    {
+        
+          nom : "Bob",
+    proprietaire :
+    {
+      nom : "Guerin",
+      prenom:"Maurice",
+      dateNaissance:"1940-04-04",
+      adresse:"Saint-Andre",
+      numero:"0643564829"
+    }, 
+    poids : 20,  
+    taille : 40, 
+    dateNaissance : "2013-12-15",
+    espece : {
+      nom : "anaconda", 
+      classe : "reptile"
+    },
+    
+    traitement :
+    [
+        
+        {
+            debut : "2019-07-5",
+            duree : 40,
+            medicament : 
+            [
+                {nom : "VITAREPTILE",
+                quantite : 3}
+                
+                
+                
+                ]
+            
+        }
+            
+            
+            
+             ]
+        
+        
+        
+        
+        
+    }
+    
+    
 )
