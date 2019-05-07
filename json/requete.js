03ab899206da2219f99a11f7c9c1191151590c19
Gia Cat <ilove3q@gmail.com>
2019-05-07 22:07:22 +0200

    update requete

--- json/requete.js
+++ json/requete.js
@@ -3,42 +3,42 @@ db = conn.getDB("Clinique");
 
 print("1) Affiche tous les animaux de Mr.Guerin : ")
 
-animaux = db.Clinique.find({"proprietaire.nom":"Guerin"}, {"nom":1,"_id":0})
+animaux = db.Clinique.find({ "proprietaire.nom": "Guerin" }, { "nom": 1, "_id": 0 })
 
-while ( animaux.hasNext() ) {
-   animal = animaux.next() ;
-   print("- ", animal.nom);	
+while (animaux.hasNext()) {
+    animal = animaux.next();
+    print("- ", animal.nom);
 }
 
 print("2) Affiche les noms de chiens : ")
 
 chiens = db.Clinique.find({ "espece.nom": "chien" }, { "_id": 0, "nom": 1 })
 
-while ( chiens.hasNext() ) {
-   chien = chiens.next() ;
-   print("- ", chien.nom);	
+while (chiens.hasNext()) {
+    chien = chiens.next();
+    print("- ", chien.nom);
 }
 
 
 
 print("3) Affiche les noms des animaux qui sont nés après 2010 : ")
 
-jeunes = db.Clinique.find({"dateNaissance":{$gte:new Date ("2010")}},{ "_id": 0, "nom": 1 })
+jeunes = db.Clinique.find({ "dateNaissance": { $gte: new Date("2010") } }, { "_id": 0, "nom": 1 })
 
 
-while ( jeunes.hasNext() ) {
-   jeune = jeunes.next() ;
-   print("- ", jeune.nom);	
+while (jeunes.hasNext()) {
+    jeune = jeunes.next();
+    print("- ", jeune.nom);
 }
 
 
 print("4) Affiche les noms des animaux sans traitement : ")
 
-non_traitements = db.Clinique.find({traitement:{$exists: false}},{ "_id": 0, "nom": 1 })
+non_traitements = db.Clinique.find({ traitement: { $exists: false } }, { "_id": 0, "nom": 1 })
 
-while ( non_traitements.hasNext() ) {
-    non_traitement = non_traitements.next() ;
-   print("- ", non_traitement.nom);	
+while (non_traitements.hasNext()) {
+    non_traitement = non_traitements.next();
+    print("- ", non_traitement.nom);
 }
 
 
@@ -73,7 +73,7 @@ while ( non_traitements.hasNext() ) {
 
 print("5) la quantité de chaque type de médicament prescrit pour Margerite")
 nomAnimal = db.Clinique.aggregate(
-    { $match:{ nom: "Margerite"}},
+    { $match: { nom: "Margerite" } },
     { $unwind: "$traitement" },
     { $unwind: "$traitement.medicament" },
     {
@@ -81,21 +81,13 @@ nomAnimal = db.Clinique.aggregate(
             _id: "$traitement.medicament.nomMolecule",
             quantite: { $sum: "$traitement.medicament.quantite" }
         }
-    }
-)
-while (nomAnimal.hasNext()){
+    })
+while (nomAnimal.hasNext()) {
     let animal = nomAnimal.next()
-    print("- ",animal._id, " quantite: ",animal.quantite)
+    print("- ", animal._id, " quantite: ", animal.quantite)
 }
 
-print("6) Nombre d' animaux et les poids et taille moyenne des animaux d'une espèce traités")
-escape = db.Clinique.aggregate({$group: {_id:"$espece.nom", poids: {$avg: "$poids"}, taille: {$avg: "$taille"},count:{$sum:1}}})
-while (escape.hasNext()){
-    let animal = escape.next()
-    print ("- ",animal._id,", nombre: ", animal.count,", poids moyenne: ",animal.poids, "taille moyenne: ",animal.taille)
-}
-
-print("7) La quantité d'un médicament prescrit au total dans la clinique")
+print("6) La quantité d'un médicament prescrit au total dans la clinique")
 medicament = db.Clinique.aggregate(
     { $unwind: "$traitement" },
     { $unwind: "$traitement.medicament" },
@@ -104,9 +96,23 @@ medicament = db.Clinique.aggregate(
             _id: "$traitement.medicament.nomMolecule",
             quantite: { $sum: "$traitement.medicament.quantite" }
         }
-    }
-)
-while (medicament.hasNext()){
+    })
+while (medicament.hasNext()) {
     let medicine = medicament.next()
-    print("- ",medicine._id, " quantite: ",medicine.quantite)
+    print("- ", medicine._id, " quantite: ", medicine.quantite)
+}
+
+print("7) Nombre d' animaux et les poids et taille moyenne des animaux d'une espèce traités")
+escape = db.Clinique.aggregate(
+    {
+        $group: {
+            _id: "$espece.nom",
+            poids: { $avg: "$poids" },
+            taille: { $avg: "$taille" },
+            count: { $sum: 1 }
+        }
+    })
+while (escape.hasNext()) {
+    let animal = escape.next()
+    print("- ", animal._id, ", nombre: ", animal.count, ", poids moyenne: ", animal.poids, "taille moyenne: ", animal.taille)
 }
