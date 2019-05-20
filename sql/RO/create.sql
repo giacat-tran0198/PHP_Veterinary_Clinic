c4eb6d4d7e9e16d58a8cf989c25fa5d4f3535d3c
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-05-20 22:33:29 +0200

    compile sauf table client

--- sql/RO/create.sql
+++ sql/RO/create.sql
@@ -1,57 +1,85 @@
-CREATE TYPE TYP_VETERINAIRE AS OBJECT
+-- Liste des veterinaires 
+CREATE OR REPLACE TYPE typ_veterinaire AS OBJECT
 (
   id  NUMBER(2),
   nom VARCHAR2(20)
 );
 /
-CREATE TABLE Espece
-(
-  nom VARCHAR2(50) PRIMARY KEY
-);
 
-CREATE TABLE ClasseEspece (
+CREATE TABLE Veterinaire OF typ_veterinaire ; 
+/ 
 
-    nom VARCHAR2(50) PRIMARY KEY
-
-)
-/
-CREATE TYPE TYP_MEDICAMENT AS OBJECT
+-- Liste des médicaaments 
+CREATE OR REPLACE TYPE typ_medicament  AS OBJECT
 (
   nomMolecule VARCHAR2(20)
 );
 /
-CREATE TYPE LISTE_MEDICAMENTS AS TABLE OF TYP_MEDICAMENT;
+
+CREATE OR REPLACE TYPE liste_medicament AS TABLE OF typ_medicament ;
 /
-CREATE TYPE TYP_TRAITEMENT AS OBJECT
+
+-- Liste des traitements suivis 
+CREATE OR REPLACE TYPE typ_traitement AS OBJECT
 (
-  veterinaire TYP_VETERINAIRE,
+  veterinaire typ_veterinaire,
   debut       DATE,
   duree       NUMBER(2),
-  medicament  TYP_MEDICAMENT
+  medicament  liste_medicament
 );
 /
-CREATE TYPE TYP_ANIMAL AS OBJECT
+
+-- Liste des epèces et classes d'annimaux 
+CREATE OR REPLACE TYPE type_classe AS OBJECT 
+( 
+  nom VARCHAR2(50)
+); 
+/ 
+
+CREATE TABLE Classe OF type_classe ; 
+/ 
+
+
+CREATE OR REPLACE TYPE type_espece AS OBJECT
+( 
+  nom VARCHAR2(20), 
+  classe REF type_classe 
+); 
+/ 
+
+CREATE TABLE Espece OF type_espece ; 
+/ 
+
+
+-- Listes d'annimaux 
+CREATE TYPE typ_animal AS OBJECT
 (
   nom           VARCHAR2(20),
   poids         FLOAT,
   taille        FLOAT,
   dateNaissance DATE,
-  espece        TYP_ESPECE,
-  traitement    TYP_TRAITEMENT
+  espece        REF type_espece, 
+  traitement    typ_traitement
 );
 /
-CREATE TYPE LISTE_ANIMAUX AS TABLE OF TYP_ANIMAL;
+
+CREATE OR REPLACE TYPE liste_animaux AS TABLE OF typ_animal;
 /
-CREATE TABLE CLIENT
-(
+
+
+
+-- Client 
+CREATE TABLE Client (
   nom           VARCHAR2(20) NOT NULL,
   prenom        VARCHAR2(20) NOT NULL,
   dateNaissance DATE         NOT NULL,
   adresse       VARCHAR2(20) NOT NULL,
   numero        VARCHAR2(10) NOT NULL,
-  animal        LISTE_ANIMAUX,
+  animal        liste_animaux,
   UNIQUE (nom, prenom, dateNaissance),
   CONSTRAINT ck_phone
     CHECK (numero LIKE '[0-9]{10}')
 )
-NESTED TABLE animal STORE AS liste_animaux;
+  NESTED TABLE animal STORE AS nt_liste_animaux ;
+/ 
+
---
+++ sql/RO/drop.sql
@@ -0,0 +1,16 @@
+DROP Client ; 
+DROP TYPE liste_animaux ; 
+DROP TYPE typ_animal ;  
+DROP TABLE Espece ; 
+DROP TYPE type_espece ; 
+DROP TABLE Classe ; 
+DROP TYPE type_classe ; 
+DROP TYPE typ_traitement ; 
+DROP TYPE liste_medicament ; 
+DROP TYPE typ_medicament ; 
+DROP TABLE Veterinaire ; 
+DROP TYPE typ_veterinaire ; 
+
+
+
+
