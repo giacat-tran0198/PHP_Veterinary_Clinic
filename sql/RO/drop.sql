e49a61dbaa1d43bc59647e9166fadd23e2d86e4f
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-05-22 19:03:28 +0200

    drop.sql modified

--- sql/RO/drop.sql
+++ sql/RO/drop.sql
@@ -1,4 +1,4 @@
-DROP Client ; 
+DROP TABLE Client ; 
 DROP TYPE liste_animaux ; 
 DROP TYPE typ_animal ;  
 DROP TABLE Espece ; 
@@ -7,9 +7,11 @@ DROP TABLE Classe ;
 DROP TYPE type_classe ; 
 DROP TYPE typ_traitement ; 
 DROP TYPE liste_medicament ; 
-DROP TYPE typ_medicament ; 
+DROP TYPE typ_medicament FORCE ; 
 DROP TABLE Veterinaire ; 
 DROP TYPE typ_veterinaire ; 
+
+-- use of DROP TYPE <type> FORCE to be sure 
 
 
 
