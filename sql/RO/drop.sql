ef844d4b445f231c681ce534487eb7724dc771e9
Gia Cat <ilove3q@gmail.com>
2019-05-22 15:42:43 +0200

    update insert and drop

--- sql/RO/drop.sql
+++ sql/RO/drop.sql
@@ -1,16 +1,13 @@
-DROP Client ; 
-DROP TYPE liste_animaux ; 
-DROP TYPE typ_animal ;  
-DROP TABLE Espece ; 
-DROP TYPE type_espece ; 
-DROP TABLE Classe ; 
-DROP TYPE type_classe ; 
-DROP TYPE typ_traitement ; 
-DROP TYPE liste_medicament ; 
-DROP TYPE typ_medicament ; 
-DROP TABLE Veterinaire ; 
-DROP TYPE typ_veterinaire ; 
-
-
-
-
+DROP TABLE Client;
+DROP TYPE LISTE_ANIMAUX;
+DROP TYPE TYP_ANIMAL;
+DROP TABLE Espece;
+DROP TYPE TYPE_ESPECE;
+DROP TABLE Classe;
+DROP TYPE TYPE_CLASSE;
+DROP TYPE LISTE_TRAITEMENTS;
+DROP TABLE Veterinaire;
+DROP TYPE TYP_TRAITEMENT;
+DROP TYPE LISTE_MEDICAMENT;
+DROP TYPE TYP_MEDICAMENT;
+DROP TYPE TYP_VETERINAIRE;
