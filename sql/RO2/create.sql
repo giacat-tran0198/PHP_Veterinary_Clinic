62482ccea5a47b512cd38b21300478e69fc08219
Anna Chevalier <anna.chevalier@etu.utc.fr>
2019-05-29 19:16:32 +0200

    Update create.sql

--- sql/RO2/create.sql
+++ sql/RO2/create.sql
@@ -1,26 +1,3 @@
--- Class Client
-
-CREATE OR REPLACE TYPE typ_client AS OBJECT (
-  id            NUMBER(2),
-  nom           VARCHAR2(20),
-  prenom        VARCHAR2(20),
-  dateNaissance DATE,
-  adresse       VARCHAR2(20),
-  numero        VARCHAR2(10)
-);
-/
-
-CREATE TABLE Client OF typ_client (
-  PRIMARY KEY (id),
-  nom           NOT NULL,
-  prenom        NOT NULL,
-  adresse       NOT NULL,
-  dateNaissance NOT NULL,
-  numero        NOT NULL,
-  UNIQUE (nom, prenom, dateNaissance),
-  CHECK (REGEXP_LIKE(numero, '^0\d{9}$'))
-);
-/
 
 -- Classe ClasseEspece
 CREATE OR REPLACE TYPE typ_ClasseEspece AS OBJECT  (
@@ -41,6 +18,15 @@ CREATE OR REPLACE TYPE  typ_Espece AS OBJECT (
 );
 /
 
+
+CREATE OR REPLACE TYPE  ref_Espece AS OBJECT (
+  refEspece REF typ_Espece
+);
+/
+
+
+CREATE OR REPLACE TYPE liste_ref_Espece AS TABLE OF ref_Espece;
+/
 CREATE TABLE Espece OF typ_Espece (
     PRIMARY KEY (nom),
     SCOPE FOR (classe) IS ClasseEspece
@@ -65,13 +51,14 @@ CREATE TABLE Veterinaire OF typ_veterinaire (
   adresse       NOT NULL ,
   numero        NOT NULL ,
   UNIQUE(nom,prenom,dateNaissance),
-  CONSTRAINT ck_phone
-    CHECK (REGEXP_LIKE(numero, '^0\d{9}$')),
+  CHECK (REGEXP_LIKE(numero, '^0\d{9}$')),
     
   SCOPE FOR (specialite) IS ClasseEspece
 
 );
 /
+
+
 
 CREATE OR REPLACE TYPE typ_assistant AS OBJECT (
   nom           VARCHAR2(50),
@@ -90,8 +77,7 @@ CREATE TABLE Assistant OF typ_assistant (
   adresse       NOT NULL ,
   numero        NOT NULL ,
   UNIQUE(nom,prenom,dateNaissance),
-  CONSTRAINT ck_phone
-    CHECK (REGEXP_LIKE(numero, '^0\d{9}$')),
+  CHECK (REGEXP_LIKE(numero, '^0\d{9}$')),
     
   SCOPE FOR (specialite) IS ClasseEspece
 
@@ -99,60 +85,117 @@ CREATE TABLE Assistant OF typ_assistant (
 /
 
 
+
+
 -- Classe Medicament
 CREATE OR REPLACE TYPE typ_medicament AS OBJECT (
   nomMolecule     VARCHAR2(20),
-  descriptionMed  VARCHAR2(255)
+  descriptionMed  VARCHAR2(255),
+  autorisation liste_ref_Espece
 );
 /
 
 CREATE TABLE Medicament OF typ_medicament (
   PRIMARY KEY (nomMolecule),
   descriptionMed NOT NULL
-);
-/
-CREATE OR REPLACE TYPE liste_medicament AS TABLE OF typ_medicament;
+  
+)NESTED TABLE autorisation STORE AS nt_autorisation;
 /
 
--- Autorisation
--- Problème avec l'unicité (medicament,espece)
-CREATE TABLE Autorisation (
-  medicament REF typ_medicament ,
-  espece     REF typ_Espece ,
-  SCOPE FOR (medicament) IS Medicament,
-  SCOPE FOR (espece) IS Espece
+
+--CREATE OR REPLACE TYPE liste_medicament AS TABLE OF typ_medicament;
+--/
+
+CREATE OR REPLACE TYPE typ_Prescription AS OBJECT (
+
+    med REF typ_medicament,
+    quantite INTEGER
+
 );
+/
+
+CREATE OR REPLACE TYPE col_Prescription AS TABLE OF typ_Prescription;
 /
 
 CREATE OR REPLACE TYPE typ_traitement AS OBJECT
 (
+  id NUMBER(2),
   veterinaire typ_veterinaire,
   debut       DATE,
   duree       NUMBER(2),
-  medicament  liste_medicament
+  prescriptions col_Prescription
 );
 /
 
-CREATE OR REPLACE TYPE liste_traitement AS TABLE OF typ_traitement;
+CREATE TABLE Traitement OF typ_traitement (
+
+    PRIMARY KEY (id),
+    veterinaire NOT NULL,
+    debut NOT NULL,
+    duree NOT NULL,
+    CHECK (duree>0)
+
+
+)NESTED TABLE prescriptions STORE AS nt_prescriptions;
 /
 
-CREATE TABLE Animal
+--CREATE OR REPLACE TYPE liste_traitement AS TABLE OF typ_traitement;
+--/
+
+CREATE OR REPLACE TYPE ref_Traitement AS OBJECT (ref_Traitement REF typ_traitement);
+/
+
+CREATE OR REPLACE TYPE liste_ref_Traitement AS TABLE OF ref_Traitement;
+/
+
+
+CREATE OR REPLACE TYPE typ_Animal AS OBJECT
 (
-  proprietaire  NUMBER(2),
   nom           VARCHAR2(20),
-  poids         FLOAT NOT NULL,
-  taille        FLOAT NOT NULL,
+  poids         FLOAT,
+  taille        FLOAT,
   dateNaissance DATE,
-  espece        VARCHAR2(50) NOT NULL,
-  traitement    liste_traitement,
-  PRIMARY KEY (proprietaire,nom),
-  FOREIGN KEY (proprietaire) REFERENCES Client(id),
-  FOREIGN KEY (espece) REFERENCES Espece(nom),
-  CONSTRAINT ck_poids
-    CHECK (poids>0),
-  CONSTRAINT ck_taille
-    CHECK (taille>0)
-)
-  NESTED TABLE traitement STORE AS nt_liste_traitement(
-  NESTED TABLE medicament STORE AS nt_liste_medicament);
+  espece        REF typ_Espece,
+  traitement    liste_ref_Traitement
+  
+);
+/
+
+
+
+CREATE TABLE Animal OF typ_Animal
+(
+  nom           NOT NULL,
+  poids         NOT NULL,
+  taille        NOT NULL,
+  espece        NOT NULL,
+
+
+ SCOPE FOR (espece) IS Espece,
+ CHECK (poids>0 AND taille>0) 
+)NESTED TABLE traitement STORE AS nt_traitements;
 / 
+
+-- Class Client
+
+CREATE OR REPLACE TYPE typ_client AS OBJECT (
+  id            NUMBER(2),
+  nom           VARCHAR2(20),
+  prenom        VARCHAR2(20),
+  dateNaissance DATE,
+  adresse       VARCHAR2(20),
+  numero        VARCHAR2(10)
+);
+/
+
+CREATE TABLE Client OF typ_client (
+  PRIMARY KEY (id),
+  nom           NOT NULL,
+  prenom        NOT NULL,
+  adresse       NOT NULL,
+  dateNaissance NOT NULL,
+  numero        NOT NULL,
+  UNIQUE (nom, prenom, dateNaissance),
+  CHECK (REGEXP_LIKE(numero, '^0\d{9}$'))
+);
+/
