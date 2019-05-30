a405ee7ff5adfed4ba70f9b0089c0098aa218cc2
Anna Chevalier <anna.chevalier@etu.utc.fr>
2019-05-30 14:52:03 +0200

    Update drop.sql

--- sql/RO2/drop.sql
+++ sql/RO2/drop.sql
@@ -2,32 +2,67 @@ DROP TABLE Animal ;
 /
 DROP TABLE Client ;
 /
+
+
+DROP TYPE typ_client FORCE;
+/
+
+DROP TYPE liste_ ref_Animal FORCE;
+/
+DROP TYPE ref_Animal FORCE;
+/
+
+
+DROP TYPE typ_Animal FORCE;
+/
+
 DROP TYPE liste_traitement FORCE;
 /
 DROP TYPE typ_traitement FORCE ;
 /
-DROP TYPE liste_medicament FORCE ;
+
+DROP TYPE col_Prescription FORCE;
+/
+
+DROP TYPE typ_Prescription FORCE;
 /
 DROP TYPE typ_medicament FORCE ;
 /
+
+DROP TYPE liste_ref_Traitement FORCE ;
+/
+DROP TYPE ref_Traitement FORCE ;
+/
+
+
+
 DROP TABLE  Espece ;
 /
 DROP TABLE  ClasseEspece ;
 /
 DROP TYPE typ_veterinaire FORCE ;
 /
+DROP TYPE typ_assistant FORCE ;
+/
 DROP TYPE typ_client FORCE ;
 /
 DROP TYPE typ_ClasseEspece FORCE ;
 /
+DROP TYPE liste_ref_Espece FORCE ;
+/
+DROP TYPE ref_Espece FORCE ;
+/
+
 DROP TYPE typ_Espece FORCE ;
 /
-DROP TABLE Autorisation;
-/
+
 DROP TABLE Medicament;
 /
 DROP TABLE Veterinaire;
 /
+DROP TABLE Assistant;
+/
+
 DROP SEQUENCE id_client_seq;
 /
 DROP SEQUENCE id_vet_seq;
