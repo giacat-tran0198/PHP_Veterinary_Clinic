12cb5ed5410daba21486146fa8fc6daa07ed5b19
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-05-30 20:44:36 +0200

    select et V1.0

--- sql/RO2/requete.sql
+++ sql/RO2/requete.sql
@@ -7,3 +7,8 @@ SELECT * FROM Espece ;
 SELECT  A.proprietaire, A.nom , A.poids, A.taille, A.dateNaissance, A.espece, t.veterinaire.nom , t.debut , t.duree, m.nomMolecule , m.quantite 
 FROM Animal A , TABLE(A.traitement) t , TABLE(t.medicament) m
 / 
+
+
+SELECT  C.id , C.nom , C.prenom , C.adresse , C.dateNaissance , C.numero 
+FROM Client C , TABLE(C.animaux) A ; 
+/ 
