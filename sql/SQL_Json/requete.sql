261074415840189fd6266953c1171b13b7bc909c
Gia Cat <ilove3q@gmail.com>
2019-06-12 17:08:24 +0200

    update requete

---
+++ sql/SQL_Json/requete.sql
@@ -0,0 +1,12 @@
+-- Liste de tous les médicaments pris par un animal
+SELECT a.nom, m->>'nomMolecule' AS medicaments
+FROM animal a, JSON_ARRAY_ELEMENTS(a.traitement) t,JSON_ARRAY_ELEMENTS(t->'medicament') m ;
+
+-- Liste des vétérinaires qui ont prescrit un traitement a l'animal en question
+SELECT a.nom, t->'veterinaire'->>'nom' AS veterinaire
+FROM animal a, JSON_ARRAY_ELEMENTS(a.traitement) t ;
+
+-- Tous les a,nimaux qui se sont fait traiter par Potter
+SELECT a.nom
+FROM animal a, JSON_ARRAY_ELEMENTS(a.traitement) t
+WHERE t->'veterinaire'->>'nom' = 'Potter';
