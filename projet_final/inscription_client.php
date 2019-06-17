b2560a894fb9fb015338aa499580bb67c0b034ae
BDD0 Etudiant <bdd0p021@smestaens24ab.sme.utc>
2019-06-17 15:22:34 +0200

    insertion donnee

--- projet_final/inscription_client.php
+++ projet_final/inscription_client.php
@@ -7,47 +7,28 @@
     <body>
         <h1>L'inscription Client</h1>
         <form action="insert_client.php" method="POST">
-            <table cellpadding="0" cellspacing="0" border="1">
-                <tr>
-                    <td>
-                        Nom :
-                    </td>
-                    <td>
-                        <input type="text" name="nom" size="50" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Prenom :
-                    </td>
-                    <td>
-                        <input type="text" name="prenom" size="50" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Date Naissance :
-                    </td>
-                    <td> <input type="date" name="dateNaissance">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        adresse :
-                    </td>
-                    <td>
-                        <input type="text" name="adresse" size="50" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        numero :
-                    </td>
-                    <td>
-                        <input type="text" name="numero" size="10" />
-                    </td>
-                </tr>
-            </table>
+          <table cellpadding="0" cellspacing="0" border="1">
+                  <tr>
+                      <td>Nom :</td>
+                      <td><input type="text" name="nom" size="50" /></td>
+                  </tr>
+                  <tr>
+                    <td> Prenom :</td>
+                    <td><input type="text" name="prenom" size="50" /></td>
+                  </tr>
+                  <tr>
+                      <td>Date Naissance :</td>
+                      <td> <input type="date" name="dateNaissance"></td>
+                  </tr>
+                  <tr>
+                      <td>Adresse :</td>
+                      <td><input type="text" name="adresse" size="50" /></td>
+                  </tr>
+                  <tr>
+                      <td>Numero :</td>
+                      <td><input type="text" name="numero" size="10" /></tr>
+                </td>
+              </table>
             <input type="submit" value="submit" />
             <input type="reset" value="réinitialiser" />
             <button onclick="history.go(-1)">Retour</button>
--- projet_final/insert_client.php
+++ projet_final/insert_client.php
@@ -1,10 +1,15 @@
-<?php 
+<!-- Accents -->
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<?php
 
 /** Connexion **/
 require('connect.php');
 
-/** Préparation et exécution de la requête **/
+/** Periode de temps par defaut **/
+date_default_timezone_set("Europe/Berlin");
 
+/** Préparation et exécution de la requête **/
 $nom = addslashes($_POST['nom']);
 $prenom = addslashes($_POST['prenom']);
 $dateNaissance = date('Y-m-d', strtotime(addslashes($_POST['dateNaissance'])));
@@ -13,15 +18,18 @@ $numero = addslashes($_POST['numero']);
 
 
 $vSql ="INSERT INTO Clinique.Client(nom,prenom,dateNaissance,adresse,numero) VALUES('{$nom}','{$prenom}','{$dateNaissance}','{$adresse}','{$numero}')";
-echo $vSql;
+//echo $vSql;
 $vSt = $vConn->prepare($vSql);
 $vSt->execute();
-if ($vSt) {
-    echo '<br> Nouveau inséré';
+if (!$vSt) {
+    echo '<br>Un nouveau client a été inseré';
+    //echo $vSt ;
   }
   else {
-    echo '<br> Erreur lors de l\'insertion';
+    echo '<br>Erreur lors de l\'insertion une contrainte n\' pas été respecté !<br>';
+    print_r($vConn->errorInfo()[2]);
   }
 echo '<br> <button onclick="history.go(-1)">Retour</button>';
 
+//$vConn->closeCursor();
 ?>
