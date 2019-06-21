96e19990c95eab596946b8719114b8f442fcd970
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-06-21 14:44:35 +0200

    JSON SQL all good

--- sql/SQL_Json/requete.sql
+++ sql/SQL_Json/requete.sql
@@ -7,6 +7,6 @@ SELECT a.nom, t->'veterinaire'->>'nom' AS veterinaire
 FROM animal a, JSON_ARRAY_ELEMENTS(a.traitement) t ;
 
 -- Tous les a,nimaux qui se sont fait traiter par Potter
-SELECT a.nom
+SELECT a.nom AS nom_animal_traite_par_POTTER
 FROM animal a, JSON_ARRAY_ELEMENTS(a.traitement) t
 WHERE t->'veterinaire'->>'nom' = 'Potter';
